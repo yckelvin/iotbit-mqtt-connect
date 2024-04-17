2204431745afce2d73d897fe51eeb03d1902a9f2
Kelvin Lee <yckelvin@users.noreply.github.com>
2024-04-18 01:06:25 +0800

    use show string without line

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     value = randint(0, 10)
     ESP8266_IoT.publishMqttMessage(convertToText(value), "myhome/null/bedroom-light", ESP8266_IoT.QosList.Qos2)
     OLED.clear()
-    OLED.writeStringNewLine("Publish value: ")
+    OLED.writeString("Publish value: ")
     OLED.writeNumNewLine(value)
 })
 let value = 0
